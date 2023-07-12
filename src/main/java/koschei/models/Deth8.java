b3a5fc5e7f3e90ab725d727ab18ce917e2824930
Иван <ivanzhurenko@gmail.com>
2023-07-12 14:41:20 +0500

    Выполнил задание 2.1.4

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,9 @@
 package koschei.models;
 
 public class Deth8 {
+
+    @Override
+    public String toString() {
+        return "Скорее всего, игла не была бы его смертью, если бы он выбрал более стерильное место для хранения";
+    }
 }
